46038287827d4c195f2c8720b418ed66168e1302
shimanshunarwalll9-create <shimanshunarwalll9@gmail.com>
2026-09-26 07:46:45 +0530

    Update [...path].ts

--- api/[...path].ts
+++ api/[...path].ts
@@ -1,5 +1,5 @@
 import type { Request, Response } from 'express';
-import app from '../server';
+import app from '../backend/index.js";
 
 export default function handler(req: Request, res: Response) {
   return app(req, res);
